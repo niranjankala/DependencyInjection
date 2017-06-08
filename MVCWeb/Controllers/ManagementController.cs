2170696c0d225a8500e29949232dac50cde4f990
Niranjan Kala <niranjan.kala@gmail.com>
2017-06-08 06:28:09 +0530

    Added some code from MS Unity

---
+++ MVCWeb/Controllers/ManagementController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCWeb.Controllers
+{  
+    public class ManagementController : Controller
+    {
+        private readonly ITenantStore tenantStore;
+        public ManagementController(ITenantStore tenantStore)
+        {
+            this.tenantStore = tenantStore;
+        }
+        public ActionResult Index()
+        {
+            var model = new TenantPageViewData<IEnumerable<string>>(this.tenantStore.GetTenantNames())
+            {
+                Title = "Subscribers"
+            };
+            return this.View(model);
+        }
+...
+}
+}
---
+++ MVCWeb/Models/Class1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVCWeb.Models
+{
+    public interface ITenantStore
+    {
+        void Initialize();
+        Tenant GetTenant(string tenant);
+        IEnumerable<string> GetTenantNames();
+        void SaveTenant(Tenant tenant);
+    }
+
+    public class Tenant
+    {
+    }
+
+    public interface ITenantStoreLogo
+    {
+        void UploadLogo(string tenant, byte[] logo);
+    }
+    public class TenantStore : ITenantStore, ITenantStoreLogo
+    {
+
+        public TenantStore()
+        {
+
+        }
+
+        public Tenant GetTenant(string tenant)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<string> GetTenantNames()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Initialize()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SaveTenant(Tenant tenant)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
